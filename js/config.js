b1ce1c7f57b27554fa526f9dea41aad8002f5308
PopCalendar <breierpop@gmail.com>
2026-09-22 16:00:17 -0700

    Update alumni URL in config.js

--- js/config.js
+++ js/config.js
@@ -17,7 +17,7 @@ const SITE_CONFIG = {
     photos: "PLACEHOLDER_PHOTOS_CSV_URL",
     coaches: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQGiToWoxDDXDsBQq_xYTBRGJrevXES0PpkgGFhdVzWfUOtJDoFwSKanfZ1S0s2iYX-5rvP6SDr2IqA/pub?gid=356609670&single=true&output=csv",
     news: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQGiToWoxDDXDsBQq_xYTBRGJrevXES0PpkgGFhdVzWfUOtJDoFwSKanfZ1S0s2iYX-5rvP6SDr2IqA/pub?gid=1332022983&single=true&output=csv",
-    alumni: "PLACEHOLDER_ALUMNI_CSV_URL"
+    alumni: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQGiToWoxDDXDsBQq_xYTBRGJrevXES0PpkgGFhdVzWfUOtJDoFwSKanfZ1S0s2iYX-5rvP6SDr2IqA/pub?gid=1513896004&single=true&output=csv"
   },
 
   // External stats provider — link to the team's MaxPreps or GameChanger page.
