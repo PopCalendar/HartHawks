8395f2d65e59b51e40fd82f5c6638608488c25af
PopCalendar <breierpop@gmail.com>
2026-09-25 13:11:18 -0700

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -19,7 +19,7 @@ const SITE_CONFIG = {
     alumni: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQGiToWoxDDXDsBQq_xYTBRGJrevXES0PpkgGFhdVzWfUOtJDoFwSKanfZ1S0s2iYX-5rvP6SDr2IqA/pub?gid=1513896004&single=true&output=csv",
     pressRoom: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQGiToWoxDDXDsBQq_xYTBRGJrevXES0PpkgGFhdVzWfUOtJDoFwSKanfZ1S0s2iYX-5rvP6SDr2IqA/pub?gid=1295516705&single=true&output=csv",
     archivedClippings: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQGiToWoxDDXDsBQq_xYTBRGJrevXES0PpkgGFhdVzWfUOtJDoFwSKanfZ1S0s2iYX-5rvP6SDr2IqA/pub?gid=1213269122&single=true&output=csv",
-    events: "PLACEHOLDER_EVENTS_CSV_URL"
+    events: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQGiToWoxDDXDsBQq_xYTBRGJrevXES0PpkgGFhdVzWfUOtJDoFwSKanfZ1S0s2iYX-5rvP6SDr2IqA/pub?gid=222442044&single=true&output=csv"
   },
 
   // External stats provider — link to the team's MaxPreps or GameChanger page.
